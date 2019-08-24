9d56a83c9c0e27178b756ef946f848965f16744a
dytra <vancielnugraha@gmail.com>
2019-08-24 17:07:05 +0700

    add more tests
    add tests: getAllPokemon(), getPokemonInfo()

--- src/tests/PokeApi.spec.js
+++ src/tests/PokeApi.spec.js
@@ -1,6 +1,15 @@
 import PokeApi from "../PokeApi";
 
 describe("PokeApi", () => {
+  test("getAllPokemon", async () => {
+    const pokemons = await PokeApi.getAllPokemon();
+    expect(pokemons.length).toBeGreaterThanOrEqual(20);
+  });
+
+  test("pokemonInfo", async () => {
+    const pokemon = await PokeApi.getPokemonInfo("pikachu");
+    expect(typeof pokemon).toBe("object");
+  });
   test("catch pokemon", () => {
     expect(typeof PokeApi.catchPokemon()).toBe("boolean");
   });
