804a9be1dba467ce9aad1a5b00817417f6c4a657
dytra <vancielnugraha@gmail.com>
2019-08-24 13:16:28 +0700

    change pokedex to pokecatch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = ({ selectedPokemon }) => {
-  const headerTitle = selectedPokemon.name ? selectedPokemon.name : "Pokédex";
+  const headerTitle = selectedPokemon.name ? selectedPokemon.name : "Pokécatch";
   return (
     <header>
       <h1>{headerTitle}</h1>
